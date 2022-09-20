ea0bbd7dacc812cded4f64a43e47d9b739994b27
harveybland <harvey.bland@genius.online>
2022-09-20 16:29:46 +0100

    Styling updates

--- src/app/ui/home/modal/modal.component.ts
+++ src/app/ui/home/modal/modal.component.ts
@@ -1,3 +1,4 @@
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialogRef: MatDialogRef<ModalComponent>) { }
 
   ngOnInit() {
   }
+
 
 }
