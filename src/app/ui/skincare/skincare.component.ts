3d3c5b7014e4a5a98906c0d738832f9ef2468a5a
harveybland <harvey.bland@genius.online>
2022-10-09 21:14:48 +0100

    skincare

--- src/app/ui/skincare/skincare.component.ts
+++ src/app/ui/skincare/skincare.component.ts
@@ -11,6 +11,40 @@ export class SkincareComponent implements OnInit {
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
+    this.animation();
   }
 
+  animation() {
+    const targets = document.querySelectorAll('.animation');
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver(callback, options);
+
+    targets.forEach((target) => {
+      observer.observe(target);
+    });
+
+    function callback(entries: any, observer: any) {
+      entries.forEach((entry: any) => {
+        if (entry.isIntersecting) {
+          addActiveClass(entry.target);
+        } else if (!entry.isIntersecting) {
+          removeActiveClass(entry.target);
+        }
+      });
+    }
+
+    function addActiveClass(element: any) {
+      element.classList.add('on');
+    }
+
+    function removeActiveClass(element: any) {
+      element.classList.remove('on');
+    }
+  }
 }
