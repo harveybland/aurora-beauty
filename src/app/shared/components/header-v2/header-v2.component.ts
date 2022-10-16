459dd4a7d3b0cb1aeca70b451d2572424e8344ba
harveybland <harvey.bland@genius.online>
2022-10-16 15:12:29 +0100

    updates

--- src/app/shared/components/header-v2/header-v2.component.ts
+++ src/app/shared/components/header-v2/header-v2.component.ts
@@ -19,11 +19,11 @@ export class HeaderV2Component implements OnInit {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 50) {
       document.querySelector('.nav')!.classList.add('nav-color');
       document.querySelectorAll('.nav-links li').forEach(el => el!.classList.add('nav-link-color'));
-      document.querySelector('.contact-button')!.classList.add('contact-button-color');
+      // document.querySelector('.contact-button')!.classList.add('contact-button-color');
     } else {
       document.querySelector('.nav')!.classList.remove('nav-color');
       document.querySelectorAll('.nav-links li').forEach(el => el!.classList.remove('nav-link-color'))
-      document.querySelector('.contact-button')!.classList.remove('contact-button-color');
+      // document.querySelector('.contact-button')!.classList.remove('contact-button-color');
     }
   }
 
