da436160e46a0956672eb946f25e3045db0ca119
harveybland <harvey.bland@genius.online>
2022-11-02 13:59:30 +0000

    refactor

--- src/app/ui/about/about.component.ts
+++ src/app/ui/about/about.component.ts
@@ -1,3 +1,4 @@
+import { AnimationService } from './../../shared/animation.service';
 import { Title } from '@angular/platform-browser';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,47 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
 
-  constructor(private titleService: Title) {
+  constructor(private titleService: Title,
+    private _animation: AnimationService) {
     this.titleService.setTitle("About Us - Aurora Beauty");
   }
 
   ngOnInit() {
     window.scrollTo(0, 0);
-    this.animation()
-  }
-
-  animation() {
-    const targets = document.querySelectorAll('.employee-wrapper');
-
-    const options = {
-      root: null,
-      rootMargin: '0px',
-      threshold: 0,
-    };
-
-    const observer = new IntersectionObserver(callback, options);
-
-    targets.forEach((target) => {
-      observer.observe(target);
-    });
-
-    function callback(entries: any, observer: any) {
-      entries.forEach((entry: any) => {
-        if (entry.isIntersecting) {
-          addActiveClass(entry.target);
-        } else if (!entry.isIntersecting) {
-          removeActiveClass(entry.target);
-        }
-      });
-    }
-
-    function addActiveClass(element: any) {
-      element.classList.add('on');
-    }
-
-    function removeActiveClass(element: any) {
-      element.classList.remove('on');
-    }
+    this._animation.animation()
   }
 
 }
