b7a310691b5e69b119aaa7c05fc9df826d58573d
harveybland <harvey.bland@genius.online>
2023-01-12 09:45:39 +0000

    text size

--- src/app/ui/nails/nails.component.ts
+++ src/app/ui/nails/nails.component.ts
@@ -5,19 +5,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nails',
   templateUrl: './nails.component.html',
-  styleUrls: ['./nails.component.scss']
+  styleUrls: ['./nails.component.scss'],
 })
 export class NailsComponent implements OnInit {
-
-  constructor(private titleService: Title,
-    private _animation: AnimationService) {
-    this.titleService.setTitle("Nails & Feet - Aurora Beauty");
+  constructor(
+    private titleService: Title,
+    private _animation: AnimationService
+  ) {
+    this.titleService.setTitle('Hands & Feet - Aurora Beauty');
   }
 
   ngOnInit() {
     window.scrollTo(0, 0);
     this._animation.animation();
   }
-
-
 }
