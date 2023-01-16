101a1fa4d54a712e092d29f1383640c09248a294
harveybland <harvey.bland@genius.online>
2023-01-16 20:19:50 +0000

    email working

--- src/app/ui/contact/contact.component.ts
+++ src/app/ui/contact/contact.component.ts
@@ -5,10 +5,9 @@ import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-
   // AIzaSyCy3Cj9bnrcOQlHAWo3YF_4wzLGag6uQZ8
 
   zoom = 15;
@@ -18,7 +17,7 @@ export class ContactComponent implements OnInit {
     scrollwheel: false,
     mapTypeControl: false,
     streetViewControl: false,
-  }
+  };
 
   emailSent: boolean = false;
 
@@ -26,39 +25,46 @@ export class ContactComponent implements OnInit {
   markerPosition!: google.maps.LatLngLiteral;
 
   constructor(private titleService: Title) {
-    this.titleService.setTitle("Contact Us - Aurora Beauty");
+    this.titleService.setTitle('Contact Us - Aurora Beauty');
   }
 
   ngOnInit() {
     window.scrollTo(0, 0);
     this.center = {
       lat: 53.86527143576552,
-      lng: -2.3945739053711863
-    }
+      lng: -2.3945739053711863,
+    };
 
     this.markerOptions = {
       draggable: false,
       label: {
         color: 'black',
-        text: 'Aurora Beauty'
-      }
+        text: 'Aurora Beauty',
+      },
     };
 
     this.markerPosition = {
       lat: 53.86527143576552,
-      lng: -2.3945739053711863
-    }
+      lng: -2.3945739053711863,
+    };
   }
 
   public sendEmail(e: Event) {
     e.preventDefault();
-    emailjs.sendForm('service_46x836u', 'template_yqrgur2', e.target as HTMLFormElement, 'stusP_Nq081JUaNOy')
-      .then((result: EmailJSResponseStatus) => {
-        this.emailSent = true
-      }, (error: any) => {
-        console.log(error.text);
-      });
+    emailjs
+      .sendForm(
+        'service_2ypqe5q',
+        'template_yqrgur2',
+        e.target as HTMLFormElement,
+        'stusP_Nq081JUaNOy'
+      )
+      .then(
+        (result: EmailJSResponseStatus) => {
+          this.emailSent = true;
+        },
+        (error: any) => {
+          console.log(error.text);
+        }
+      );
   }
-
-
 }
