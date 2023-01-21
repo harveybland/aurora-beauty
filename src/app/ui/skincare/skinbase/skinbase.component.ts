7b33218f21b56706f1b0130f1421b604cd394f74
harveybland <harvey.bland@genius.online>
2023-01-21 20:18:25 +0000

    updates

--- src/app/ui/skincare/skinbase/skinbase.component.ts
+++ src/app/ui/skincare/skinbase/skinbase.component.ts
@@ -1,15 +1,22 @@
+import { Title } from '@angular/platform-browser';
+import { AnimationService } from './../../../shared/animation.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skinbase',
   templateUrl: './skinbase.component.html',
-  styleUrls: ['./skinbase.component.scss']
+  styleUrls: ['./skinbase.component.scss'],
 })
 export class SkinbaseComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private titleService: Title,
+    private _animation: AnimationService
+  ) {
+    this.titleService.setTitle('Skincare - Aurora Beauty');
   }
 
+  ngOnInit() {
+    window.scrollTo(0, 0);
+    this._animation.animation();
+  }
 }
